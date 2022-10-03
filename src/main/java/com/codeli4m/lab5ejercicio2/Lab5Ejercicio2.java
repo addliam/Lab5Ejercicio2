1b230e7515d29c95fdfa4a4df46de4161918234f
li4m <applecdos504@gmail.com>
2022-10-03 12:13:17 -0500

    interfaz main y reporte

---
+++ src/main/java/com/codeli4m/lab5ejercicio2/Lab5Ejercicio2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.codeli4m.lab5ejercicio2;
+
+/**
+ *
+ * @author HP
+ */
+public class Lab5Ejercicio2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
